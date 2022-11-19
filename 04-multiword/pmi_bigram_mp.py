4af3f26ce78380febdc1f301570c58540a99aa0c
Mateusz Wojtulewicz <mateusz.wojtulewicz@gmail.com>
2022-11-19 18:39:35 +0100

    multiword raport finished

--- 04-multiword/pmi_bigram_mp.py
+++ 04-multiword/pmi_bigram_mp.py
@@ -1,41 +1,44 @@
 import multiprocessing as mp
 import pickle
 from collections import Counter
+from itertools import repeat
 
 import numpy as np
-import tqdm
+from tqdm import tqdm
+
+N_PROCESSES = 4
 
 
 def load_counter(name) -> Counter:
-    with open(f"{name}.p", "rb") as f:
+    with open(f"saved/{name}.p", "rb") as f:
         return pickle.load(f)
 
 
 def dump_results(results: list, name: str) -> None:
-    with open(f"{name}.p", "wb") as f:
+    with open(f"saved/{name}.p", "wb") as f:
         pickle.dump(results, f)
 
 
-def p_bigram(bigram: tuple[str, str]) -> float:
-    return bigrams_filtered_counter[bigram] / bigrams_counter.total()
+def p_bigram(bigram: tuple[str, str], all_bigrams: Counter) -> float:
+    return all_bigrams[bigram] / all_bigrams.total()
 
 
-def p_token(tok: str) -> float:
-    return tokens_counter[tok] / tokens_counter.total()
+def p_token(tok: str, all_tokens: Counter) -> float:
+    return all_tokens[tok] / all_tokens.total()
 
 
-def pmi_bigram(bigram: tuple[str, str]) -> tuple[tuple[str, str], float]:
+def pmi_bigram(
+    bigram: tuple[str, str], all_bigrams: Counter, all_tokens: Counter
+) -> tuple[tuple[str, str], float]:
     a, b = bigram
-    p_a = p_token(tok=a)
-    p_b = p_token(tok=b)
-    p_ab = p_bigram(bigram=bigram)
+    p_a = p_token(tok=a, all_tokens=all_tokens)
+    p_b = p_token(tok=b, all_tokens=all_tokens)
+    p_ab = p_bigram(bigram=bigram, all_bigrams=all_bigrams)
     pmi = np.log2(p_ab / (p_a * p_b))
     return bigram, pmi
 
 
 if __name__ == "__main__":
-
-    mp.freeze_support()
 
     for prefix in ["", "tag_"]:
 
@@ -45,13 +48,19 @@ if __name__ == "__main__":
         )
         tokens_counter = load_counter(name=f"{prefix}tokens_counter")
 
-        pmi_results = []
-        with mp.Pool(processes=10) as pool:
-            with tqdm.tqdm(total=len(bigrams_filtered_counter)) as pbar:
-                for bigram, pmi in pool.imap_unordered(
-                    pmi_bigram, bigrams_filtered_counter
-                ):
-                    pmi_results.append((bigram, pmi))
-                    pbar.update()
+        with mp.Pool(processes=N_PROCESSES) as pool:
+            inputs = zip(
+                bigrams_filtered_counter,
+                repeat(bigrams_counter),
+                repeat(tokens_counter),
+            )
+            pmi_results = pool.starmap(
+                pmi_bigram,
+                tqdm(
+                    inputs,
+                    total=len(bigrams_filtered_counter),
+                    desc=f"Calculating PMI [{prefix}bigrams]",
+                ),
+            )
 
         dump_results(results=pmi_results, name=f"{prefix}bigram_pmi_results")
--- 04-multiword/pmi_trigram_mp.py
+++ 04-multiword/pmi_trigram_mp.py
@@ -1,42 +1,45 @@
 import multiprocessing as mp
 import pickle
 from collections import Counter
+from itertools import repeat
 
 import numpy as np
 import tqdm
 
+N_PROCESSES = 4
+
 
 def load_counter(name) -> Counter:
-    with open(f"{name}.p", "rb") as f:
+    with open(f"saved/{name}.p", "rb") as f:
         return pickle.load(f)
 
 
 def dump_results(results: list, name: str) -> None:
-    with open(f"{name}.p", "wb") as f:
+    with open(f"saved/{name}.p", "wb") as f:
         pickle.dump(results, f)
 
 
-def p_trigram(trigram: tuple[str, str, str]) -> float:
-    return trigrams_filtered_counter[trigram] / trigrams_counter.total()
+def p_trigram(trigram: tuple[str, str, str], all_trigrams: Counter) -> float:
+    return all_trigrams[trigram] / all_trigrams.total()
 
 
-def p_token(tok: str) -> float:
-    return tokens_counter[tok] / tokens_counter.total()
+def p_token(tok: str, all_tokens: Counter) -> float:
+    return all_tokens[tok] / all_tokens.total()
 
 
-def pmi_trigram(trigram: tuple[str, str, str]) -> tuple[tuple[str, str, str], float]:
+def pmi_trigram(
+    trigram: tuple[str, str, str], all_trigrams: Counter, all_tokens: Counter
+) -> tuple[tuple[str, str, str], float]:
     a, b, c = trigram
-    p_a = p_token(tok=a)
-    p_b = p_token(tok=b)
-    p_c = p_token(tok=c)
-    p_abc = p_trigram(trigram=trigram)
+    p_a = p_token(tok=a, all_tokens=all_tokens)
+    p_b = p_token(tok=b, all_tokens=all_tokens)
+    p_c = p_token(tok=c, all_tokens=all_tokens)
+    p_abc = p_trigram(trigram=trigram, all_trigrams=all_trigrams)
     pmi = np.log2(p_abc / (p_a * p_b * p_c))
     return trigram, pmi
 
 
 if __name__ == "__main__":
-
-    mp.freeze_support()
 
     for prefix in ["", "tag_"]:
 
@@ -46,13 +49,19 @@ if __name__ == "__main__":
         )
         tokens_counter = load_counter(name=f"{prefix}tokens_counter")
 
-        pmi_results = []
-        with mp.Pool(processes=10) as pool:
-            with tqdm.tqdm(total=len(trigrams_filtered_counter)) as pbar:
-                for bigram, pmi in pool.imap_unordered(
-                    pmi_trigram, trigrams_filtered_counter.keys()
-                ):
-                    pmi_results.append((bigram, pmi))
-                    pbar.update()
+        with mp.Pool(processes=N_PROCESSES) as pool:
+            inputs = zip(
+                trigrams_filtered_counter,
+                repeat(trigrams_counter),
+                repeat(tokens_counter),
+            )
+            pmi_results = pool.starmap(
+                pmi_trigram,
+                tqdm.tqdm(
+                    inputs,
+                    total=len(trigrams_filtered_counter),
+                    desc=f"Calculating PMI [{prefix}trigrams]",
+                ),
+            )
 
         dump_results(results=pmi_results, name=f"{prefix}trigram_pmi_results")
